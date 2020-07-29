8918df94eb7dd44ce62e9ad2c4102fe74a56e9be
vadim030303 <andreaimorozov@gmail.com>
2020-07-29 16:31:28 -0400

    changed view of response

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,10 @@ class Stock < ApplicationRecord
 	def self.new_lookup(ticker_symbol)
 	  client = IEX::Api::Client.new(publishable_token: 'Tpk_ee776f9a47c4473bbb5e2d5775ee18e2', secret_token: 'Tsk_475b6e3c3eda4b798950ad3a55d0a1f9',
                                     endpoint: 'https://sandbox.iexapis.com/v1')
-      client.price(ticker_symbol)
+	  begin
+	    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+	  rescue => exception
+	  	return nil 
+	  end
     end
 end
